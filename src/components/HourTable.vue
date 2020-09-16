334032621398c21959b283dfdd15589240973a9f
v_lcji(季林成) <v_lcji@tencent.com>
2020-09-16 17:07:32 +0800

    Add HourTable;

---
+++ src/components/HourTable.vue
@@ -0,0 +1,63 @@
+<template>
+  <b-table responsive borderless selectable select-mode="single" :fields="tableFields" :items="tableItems">
+    <template v-slot:cell(hour)="data">
+      {{ data.value }}
+    </template>
+    <template v-slot:cell(day0)="data" v-if="['W', 'X', 'D'].includes(timeUnit)">
+      <b-button
+        block
+        id="taskButton"
+        v-if="data.value.title"
+        :variant="importanceMap[data.value.importance]"
+      >
+        {{ data.value.title }}
+      </b-button>
+    </template>
+    <template v-slot:cell(day1)="data" v-if="['W', 'X'].includes(timeUnit)">
+      {{ data.value.title }}
+    </template>
+    <template v-slot:cell(day2)="data" v-if="['W', 'X'].includes(timeUnit)">
+      {{ data.value.title }}
+    </template>
+    <template v-slot:cell(day3)="data" v-if="['W', 'X'].includes(timeUnit)">
+      {{ data.value.title }}
+    </template>
+    <template v-slot:cell(day4)="data" v-if="['W'].includes(timeUnit)">
+      {{ data.value.title }}
+    </template>
+    <template v-slot:cell(day5)="data" v-if="['W'].includes(timeUnit)">
+      {{ data.value.title }}
+    </template>
+    <template v-slot:cell(day6)="data" v-if="['W'].includes(timeUnit)">
+      {{ data.value.title }}
+    </template>
+  </b-table>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'HourTable',
+  props: {
+    timeUnit: {
+      type: String
+    },
+    tableFields: {
+      type: Array
+    },
+    tableItems: {
+      type: Array
+    }
+  },
+  data () {
+    return {
+      importanceMap: {
+        0: 'success',
+        1: 'primary',
+        2: 'warning',
+        3: 'danger'
+      }
+    }
+  }
+})
+</script>
