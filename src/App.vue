bc577fd6348c8a84984b39e67584f46f73a1fc6d
primus2019 <primus2019@outlook.com>
2020-09-19 15:41:55 +0800

    Fix margin bugs with v-modal;

--- src/App.vue
+++ src/App.vue
@@ -13,10 +13,6 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-}
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
@@ -24,5 +20,10 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+body {
+  margin: 0;
+  padding: 0 !important;
 }
 </style>
